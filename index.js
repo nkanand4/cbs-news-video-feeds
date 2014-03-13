659951925f7ba530eb31158047c2138a0485f56a
Nitesh Kumar Anand <nitesh@Niteshs-MacBook-Pro.local>
2014-03-12 21:14:27 -0500

    Added shell script to run and get feeds. Wrapping up.

--- index.js
+++ index.js
@@ -4,8 +4,10 @@ var domain = 'www.cbsnews.com',
     jsdom = require('jsdom'),
     jquery = fs.readFileSync("./jquery.js").toString(),
     defer = require("node-promise").defer,
-    categories = ['cbs-this-morning', 'evening-news', '60-minutes', '48-hours'];
-
+    category = 'cbs-this-morning',
+    m3u = [];
+    // dummy data
+    // 'cbs-this-morning', 'evening-news', '60-minutes', '48-hours'
 
 
 function getPlaylistVideoPages(request) {
@@ -36,10 +38,12 @@ function fetchPlaylist(playlistType) {
     return getPlaylistVideoPages(request);
 }
 
-function get() {
-    fetchPlaylist(categories[2]).then(pushToJSONArray);
+function get(category) {
+    var total = 0;
+    fetchPlaylist(category).then(pushToJSONArray);
 
     function pushToJSONArray(list) {
+        total = list.length;
         list.forEach(function(item) {
             http.request({
                 hostname: domain,
@@ -49,16 +53,34 @@ function get() {
                 jsdom.env(response, ["http://code.jquery.com/jquery.js"], function(error, win) {
                     var $ = win.jQuery;
                     var state = JSON.parse($('#container-video').attr('data-cbsvideoui-options')).state;
-                    var data = {
-                        title: state.title,
-                        url: state.video.image.full,
-                        video: state.video.medias.tablet.uri
-                    };
-                    console.log('info', data);
+                    var device = state.video.medias.tablet ?
+                                    state.video.medias.tablet : state.video.medias.mobile ? state.video.medias.mobile : state.video.medias.ios;
+                    if(device) {
+                        m3u.push({
+                            title: state.title,
+                            url: state.video.image.full,
+                            video: device.uri
+                        });
+                    }
+
+                    total -= 1;
+                    if(total === 0) {
+                        writeToFile();
+                    }
                 });
             });
         });
     }
 }
 
-get();
+function writeToFile() {
+    var data = {
+        playlist : m3u
+    };
+    console.log(JSON.stringify(data));
+}
+
+if(typeof process.argv[2] !== 'undefined') {
+    category = process.argv[2];
+}
+get(category);
